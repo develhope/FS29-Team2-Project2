ca85f3b76113fdd86e3b9c926acfe763b6c5b248
Lorenzo Zannetti <zannetti.lorenzo@gmail.com>
2024-05-27 15:54:16 +0200

    function ADD TASKS

--- taskManagementSystem.js
+++ taskManagementSystem.js
@@ -1,7 +1,25 @@
 const tasks = [];
 
-function addTasks(task, callback) {}
+function addTasks(task, callback) {
+  setTimeout(() => {
+    if (typeof task === "string") {
+      tasks.push(task);
+      callback(null, "Task added successfully: " + tasks);
+    } else {
+      callback(new Error("Task must be a non empty string!!"), null);
+    }
+  }, 2000);
+}
 
 function completeTask(i, callback) {}
 
 function listTasks() {}
+
+addTasks("mangiare", (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+    console.log(tasks);
+  }
+});
