175b8152cb27d83eabce96f36e1198bbbbad8d13
Nicolas Papappicco <nicolas.papappicco@gmail.com>
2024-05-27 16:35:39 +0200

    test completeTask

--- taskManagementSystem.js
+++ taskManagementSystem.js
@@ -11,7 +11,18 @@ function addTasks(task, callback) {
   }, 2500);
 }
 
-function completeTask(i, callback) {}
+function completeTask(i, callback) {
+  setTimeout(() => {
+    if (i >= tasks.length || i < 0) {
+      callback(new Error("Invalid task index"), null);
+    } else {
+      console.log(tasks[i]);
+      tasks.filter((task) => task !== task[i]);
+      console.log(tasks);
+      callback(null, "Task completed successfully");
+    }
+  }, 3000);
+}
 
 function listTasks() {}
 
@@ -21,5 +32,13 @@ addTasks("mangiare", (error, data) => {
   } else {
     console.log(data);
     console.log(tasks);
+  }
+});
+
+completeTask(0, (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
   }
 });
